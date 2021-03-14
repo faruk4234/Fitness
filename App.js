3d562e9919bb6f3d563a8e29cfa8240c19cf2c09
faruk4234 <60069276+faruk4234@users.noreply.github.com>
2021-03-14 14:09:31 +0300

    component1

--- App.js
+++ App.js
@@ -1,33 +1,101 @@
 import React from 'react'
 
 import {
+    Text,
     View,
     Image,
     StyleSheet,
     TouchableOpacity
 } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
-const Foto = () => (
-    <View style={styles.Container}>
-        <TouchableOpacity style={styles.opacityContainer}>
-            <Image style={styles.ımageContainer} source={{ uri: 'https://blogginballz.files.wordpress.com/2016/12/kid_goku_episode_1.jpg' }} />
-        </TouchableOpacity>
-    </View>
-)
+const Foto = () => {
+    const [click, setClick] = React.useState(false)
 
+    const Profilim = () => {
+        console.log('profilim')
+    }
+
+    const Ayarlar = () => {
+        console.log('ayarlar')
+    }
+
+    return (
+        <View style={styles.Container}>
+
+            <Image
+                style={styles.ımageContainer}
+                source={{ uri: 'https://blogginballz.files.wordpress.com/2016/12/kid_goku_episode_1.jpg' }} />
+
+            <Icon
+                style={styles.ıconContainer}
+                onPress={() => setClick(!click)}
+                name={click
+                    ? 'caret-down-outline'
+                    : 'caret-up-outline'} />
+
+            { click ? (
+                <View style={styles.childContainer}>
+
+                    <TouchableOpacity
+                        style={styles.opacityBar}
+                        onPress={Profilim}>
+
+                        <Text style={styles.textConteiner1}>
+                            Profilim
+                        </Text>
+
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        style={styles.opacityBar}
+                        onPress={Ayarlar}>
+
+                        <Text style={styles.textConteiner2}>
+                            Ayarlar
+                        </Text>
+
+                    </TouchableOpacity>
+
+                </View>
+            ) : null }
+
+        </View>
+    )
+}
 const styles = StyleSheet.create({
     Container: {
         width: '23%',
         height: '13%'
     },
-    opacityContainer: {
-        width: '100%',
-        height: '100%'
-    },
     ımageContainer: {
         width: '100%',
         height: '100%',
         borderRadius: 200
+    },
+    ıconContainer: {
+        color: 'white',
+        fontSize: 24,
+        position: 'absolute',
+        top: '70%',
+        left: '36%'
+    },
+    childContainer: {
+        backgroundColor: 'black',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '70%',
+        opacity: 0.89,
+        left: '15%'
+    },
+    textConteiner1: {
+        color: 'white',
+        borderBottomWidth: 0.6,
+        borderBottomColor: 'white'
+    },
+    textConteiner2: {
+        color: 'white'
     }
 })
 
