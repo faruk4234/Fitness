b5285a9ff5af35098b13a7248c7dc65b30897dd6
faruk4234 <60069276+faruk4234@users.noreply.github.com>
2021-03-15 17:53:30 +0300

    log screen

--- src/screens/loginscreen/LoginScreen.js
+++ src/screens/loginscreen/LoginScreen.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
-export default class LoginScreen extends Component {
-    render() {
-        return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
-        )
+import BigLogo from '../../compnents/BigLogo'
+
+const LoginScreen = () => (
+    <View style={styles.container}>
+        <BigLogo />
+        <Text> GİRİŞ YAP</Text>
+    </View>
+)
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        left: '17.5%'
     }
-}
+})
+
+export default LoginScreen
